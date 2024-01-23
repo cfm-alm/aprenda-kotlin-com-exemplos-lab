3478e36d29402cd8ad18eb47cc17ccd96a6ea7ba
cfm-alm <108082812+cfm-alm@users.noreply.github.com>
2024-01-23 16:38:06 -0300

    Desafio 1

--- desafio.kt
+++ desafio.kt
@@ -1,8 +1,6 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(var nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
@@ -11,11 +9,20 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        val nome = usuario.nome
+        println("Usuario $nome matriculado")
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val user1 = Usuario("user1")
+    val user2 = Usuario("user2")
+    
+    val lista_conteudo = listOf(ConteudoEducacional("math"), ConteudoEducacional("SCIENCE"))
+    
+    val form = Formacao("f1", lista_conteudo)
+    form.matricular(user1)
+    form.matricular(user2)
 }
